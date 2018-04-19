1f543ea791074f18a3df14844db88ac04d31fb73
Bo Huang <bhuang1228@gmail.com>
2018-04-18 22:47:15 -0400

    finish FastCollinearPoints.java

--- AlgorithmsPartOne/WeekThree/Assignment/FastCollinearPoints.java
+++ AlgorithmsPartOne/WeekThree/Assignment/FastCollinearPoints.java
@@ -6,6 +6,7 @@ import java.util.List;
 public class FastCollinearPoints {
   private Point[] points;
   private List<LineSegment> segments;
+  private List<Double> slopes;
 
   // finds all line segments containing 4 or more points
   public FastCollinearPoints(Point[] points) {
@@ -15,6 +16,7 @@ public class FastCollinearPoints {
 
     this.points = points;
     this.segments = new ArrayList<LineSegment>();
+    this.slopes = new ArrayList<Double>();
 
     this.findSegments();
   }
@@ -38,24 +40,34 @@ public class FastCollinearPoints {
 
       Point start = list.get(1);
       int count = 1;
-      double slope = point.slopeTo(start);
+      double slope1 = point.slopeTo(start);
+      double slope2 = 0.0;
 
       for (int i = 2; i < list.size(); i++) {
-        if (slope == point.slopeTo(list.get(i))) {
+        slope2 = point.slopeTo(list.get(i));
+
+        if (slope1 == slope2) {
           count++;
         } else {
           if (count >= 3) {
-            this.segments.add(new LineSegment(start, list.get(i - 1)));
+            this.addSegment(slope1, start, list.get(i - 1));
             start = list.get(i - 1);
             count = 1;
-            slope = point.slopeTo(list.get(i));
+            slope1 = slope2;
           }
         }
       }
 
       if (count >= 3) {
-        this.segments.add(new LineSegment(start, list.get(list.size() - 1)));
+        this.addSegment(slope1, start, list.get(list.size() - 1));
       }
+    }
+  }
+
+  private void addSegment(double slope, Point p1, Point p2) {
+    if (this.slopes.indexOf(slope) >= 0) {
+      this.slopes.add(slope);
+      this.segments.add(new LineSegment(p1, p2));
     }
   }
 }
