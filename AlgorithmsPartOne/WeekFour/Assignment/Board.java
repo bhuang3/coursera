115314ed5d0af41e441d1e9e0dd30f4b1120e540
Bo Huang <bhuang1228@gmail.com>
2018-04-30 23:21:13 -0400

    fix bug

---
+++ AlgorithmsPartOne/WeekFour/Assignment/Board.java
@@ -0,0 +1,180 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Board {
+  private final int[][] blocks;
+  private int hammingPriority;
+  private int manhattanPriority;
+  private int emptyX;
+  private int emptyY;
+  private String stringValue;
+
+  // construct a board from an n-by-n array of blocks
+  // (where blocks[i][j] = block in row i, column j)
+  public Board(int[][] blocks) {
+    this.blocks = blocks.clone();
+
+    this.initBoard();
+  }
+
+  // board dimension n
+  public int dimension() {
+    return this.blocks.length;
+  }
+
+  // number of blocks out of place
+  public int hamming() {
+    return this.hammingPriority;
+  }
+
+  // sum of Manhattan distances between blocks and goal
+  public int manhattan() {
+    return this.manhattanPriority;
+  }
+
+  // is this board the goal board?
+  public boolean isGoal() {
+    return this.hammingPriority == 0 && this.manhattanPriority == 0;
+  }
+
+  // a board that is obtained by exchanging any pair of blocks
+  public Board twin() {
+    int n = this.blocks.length;
+    int[][] twinBlocks = this.cloneBlocks(this.blocks);
+    int x = -1;
+    int y = -1;
+
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n; j++) {
+        if (twinBlocks[i][j] != 0) {
+          if (x == -1) {
+            x = i;
+            y = j;
+          } else {
+            swap(twinBlocks, i, j, x, y);
+
+            return new Board(twinBlocks);
+          }
+        }
+      }
+    }
+
+    return null;
+  }
+
+  // does this board equal y?
+  public boolean equals(Object y) {
+    if (y == this) return true;
+    if (y == null) return false;
+    if (y.getClass() != this.getClass()) return false;
+
+    Board that = (Board) y;
+
+    for (int i = 0; i < this.blocks.length; i++) {
+      for (int j = 0; j < this.blocks.length; j++) {
+        if (this.blocks[i][j] != that.blocks[i][j]) {
+          return false;
+        }
+      }
+    }
+
+    return true;
+  }
+
+  // all neighboring boards
+  public Iterable<Board> neighbors() {
+    List<Board> boards = new ArrayList<Board>();
+    int n = this.blocks.length;
+
+    // top
+    if (this.emptyX - 1 >= 0) {
+      int[][] top = this.cloneBlocks(this.blocks);
+      swap(top, this.emptyX, this.emptyY, this.emptyX - 1, this.emptyY);
+      boards.add(new Board(top));
+    }
+
+    // bottom
+    if (this.emptyX + 1 < n) {
+      int[][] bottom = this.cloneBlocks(this.blocks);
+      swap(bottom, this.emptyX, this.emptyY, this.emptyX + 1, this.emptyY);
+      boards.add(new Board(bottom));
+    }
+
+    // left
+    if (this.emptyY - 1 >= 0) {
+      int[][] left = this.cloneBlocks(this.blocks);
+      swap(left, this.emptyX, this.emptyY, this.emptyX, this.emptyY - 1);
+      boards.add(new Board(left));
+    }
+
+    // right
+    if (this.emptyY + 1 < n) {
+      int[][] right = this.cloneBlocks(this.blocks);
+      swap(right, this.emptyX, this.emptyY, this.emptyX, this.emptyY + 1);
+      boards.add(new Board(right));
+    }
+
+    return boards;
+  }
+
+  // string representation of this board (in the output format specified below)
+  public String toString() {
+    return this.stringValue;
+  }
+
+  private void initBoard() {
+    int hamming = 0;
+    int manhattan = 0;
+    int n = this.blocks.length;
+    StringBuilder sb = new StringBuilder();
+
+    sb.append(n).append("\n");
+
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n; j++) {
+        int expect = i * n + j + 1;
+        int actual = blocks[i][j] == 0 ? n * n : blocks[i][j];
+
+        sb.append(String.format("%2d ", this.blocks[i][j]));
+
+        if (blocks[i][j] == 0) {
+          emptyX = i;
+          emptyY = j;
+        }
+
+        if (expect != actual) {
+          int distance = Math.abs(actual - expect);
+
+          hamming++;
+          manhattan += distance / n + distance % n;
+        }
+      }
+
+      sb.append("\n");
+    }
+
+    this.hammingPriority = hamming;
+    this.manhattanPriority = manhattan;
+    this.stringValue = sb.toString();
+  }
+
+  private int[][] cloneBlocks(int[][] b) {
+    int n = b.length;
+    int[][] newBlocks = new int[n][n];
+
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n; j++) {
+        newBlocks[i][j] = b[i][j];
+      }
+    }
+
+    return newBlocks;
+  }
+
+  private void swap(int[][] b, int i1, int j1, int i2, int j2) {
+    int temp = b[i1][j1];
+
+    b[i1][j1] = b[i2][j2];
+    b[i2][j2] = temp;
+  }
+}
---
+++ AlgorithmsPartOne/WeekFour/Assignment/Solver.java
@@ -0,0 +1,128 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+import edu.princeton.cs.algs4.MinPQ;
+
+public class Solver {
+  private final MinPQ<SearchNode> queue;
+  private final MinPQ<SearchNode> queueTwin;
+  private final List<Board> boards;
+  private boolean solvable;
+
+  // find a solution to the initial board (using the A* algorithm)
+  public Solver(Board initial) {
+    if (initial == null) {
+      throw new IllegalArgumentException();
+    }
+
+    this.queue = new MinPQ<SearchNode>(new SearchNodeComparator());
+    this.queueTwin = new MinPQ<SearchNode>(new SearchNodeComparator());
+    this.boards = new ArrayList<Board>();
+    this.solvable = false;
+
+    this.solve(initial);
+  }
+
+  // is the initial board solvable?
+  public boolean isSolvable() {
+    return this.solvable;
+  }
+
+  // min number of moves to solve initial board; -1 if unsolvable
+  public int moves() {
+    if (this.isSolvable()) {
+      return this.boards.size() - 1;
+    } else {
+      return -1;
+    }
+  }
+
+  // sequence of boards in a shortest solution; null if unsolvable
+  public Iterable<Board> solution() {
+    if (this.isSolvable()) {
+      return this.boards;
+    } else {
+      return null;
+    }
+  }
+
+  private void solve(Board board) {
+    Board twin = board.twin();
+    SearchNode min = null;
+    SearchNode minTwin = null;
+
+    this.queue.insert(new SearchNode(0, board, null));
+    this.queueTwin.insert(new SearchNode(0, twin, null));
+
+    while (true) {
+      min = this.queue.delMin();
+      minTwin = this.queueTwin.delMin();
+
+      if (min.board.isGoal()) {
+        this.solvable = true;
+        break;
+      } else {
+        for (Board neighbor : min.board.neighbors()) {
+          if (!this.isInQueue(min, neighbor)) {
+            this.queue.insert(new SearchNode(min.moves + 1, neighbor, min));
+          }
+        }
+      }
+
+      if (minTwin.board.isGoal()) {
+        break;
+      } else {
+        for (Board neighbor : minTwin.board.neighbors()) {
+          if (!this.isInQueue(minTwin, neighbor)) {
+            this.queueTwin.insert(new SearchNode(minTwin.moves + 1, neighbor, minTwin));
+          }
+        }
+      }
+    }
+
+    if (this.solvable) {
+      this.boards.clear();
+
+      while (min != null) {
+        this.boards.add(0, min.board);
+        min = min.parent;
+      }
+    }
+  }
+
+  private boolean isInQueue(SearchNode node, Board board) {
+    while (node != null) {
+      if (board.equals(node.board)) {
+        return true;
+      }
+
+      node = node.parent;
+    }
+
+    return false;
+  }
+
+  private class SearchNode {
+    private int moves;
+    private Board board;
+    private SearchNode parent;
+
+    private SearchNode(int moves, Board board, SearchNode parent) {
+      this.moves = moves;
+      this.board = board;
+      this.parent = parent;
+    }
+
+    public int getPriority() {
+      return this.board.manhattan() + this.moves;
+    }
+  }
+
+  private class SearchNodeComparator implements Comparator<SearchNode> {
+    @Override
+    public int compare(SearchNode node1, SearchNode node2) {
+      return node1.getPriority() - node2.getPriority();
+    }
+  }
+}
